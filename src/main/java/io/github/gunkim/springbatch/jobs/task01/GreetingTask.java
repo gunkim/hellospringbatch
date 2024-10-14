874f9a3639229fcbbd46c456e91d29ad5df37250
gunkim <gunkim.dev@gmail.com>
2024-10-15 03:23:59 +0900

    :sparkles: spring batch의 Job, Step 등록

---
+++ src/main/java/io/github/gunkim/springbatch/jobs/task01/GreetingTask.java
@@ -0,0 +1,24 @@
+package io.github.gunkim.springbatch.jobs.task01;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.InitializingBean;
+
+@Slf4j
+public class GreetingTask implements Tasklet, InitializingBean {
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+        log.info("------------------------ Task Execute -------------------");
+        log.info("GreetingTask: {}, {}", contribution, chunkContext);
+
+        return RepeatStatus.FINISHED;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        log.info("------------------------ After Properties Sets() -------------------");
+    }
+}
